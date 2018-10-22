d2d2828fcb6dd013368acc2736689e2529bee2f9
Luciano Santos <luciano_z7@hotmail.com>
2018-10-22 12:36:54 -0300

    Introduction to console

---
+++ console_module/console_count.js
@@ -0,0 +1,9 @@
+//This code counts te score of remo, rj and
+//default score with gows to none of them
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.count('remo');
+console.count('remo');
+console.count('rj');
+console.count();
